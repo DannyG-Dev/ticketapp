9c599c725250f4f66fe8af90e783e3839cdb7bdd
Danny <72071917+DannyG-Dev@users.noreply.github.com>
2024-02-19 22:53:27 +0000

    Added Ticket Table, Ticket Form for New Tickets, Form Validation and styling

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import prisma from "@/prisma/db";
+import TicketTable from "./TicketTable";
 
 const Tickets = async () => {
   const tickets = await prisma.ticket.findMany();
 
   console.log(tickets);
-  return <div>Tickets</div>;
+  return (
+    <div>
+      <TicketTable tickets={tickets} />
+    </div>
+  );
 };
 
 export default Tickets;
---
+++ components/TicketStatusBadge.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Badge } from "./ui/badge";
+import { Status } from "@prisma/client";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  {
+    label: string;
+    color: "bg-red-400" | "bg-blue-400" | "bg-green-400" | "bg-orange-400";
+  }
+> = {
+  OPEN: { label: "Open", color: "bg-orange-400" },
+  CLOSED: { label: "Closed", color: "bg-green-400" },
+  STARTED: { label: "Started", color: "bg-blue-400" },
+  REJECTED: { label: "Rejected", color: "bg-red-400" },
+};
+
+const TicketStatusBadge = ({ status }: Props) => {
+  return (
+    <Badge
+      className={`${statusMap[status].color} hover:${statusMap[status].color} text-background`}
+    >
+      {statusMap[status].label}
+    </Badge>
+  );
+};
+
+export default TicketStatusBadge;
